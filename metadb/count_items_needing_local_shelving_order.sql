bec94ca13c55e603fe732ef05cb2117d47ab89f4
Maccabee Levine <msl321@lehigh.edu>
2025-05-21 08:17:39 -0400

    Count items needing local shelving order

---
+++ metadb/count_items_needing_local_shelving_order.sql
@@ -0,0 +1,35 @@
+-- metadb:function count_items_needing_local_shelving_order
+-- Count the items that would be returned from get_items_needing_local_shelving_order.
+
+DROP FUNCTION IF EXISTS count_items_needing_local_shelving_order;
+
+CREATE FUNCTION count_items_needing_local_shelving_order(
+)
+RETURNS TABLE (
+	total BIGINT
+)
+AS
+$$
+SELECT
+	COUNT(*)
+FROM
+    folio_inventory.item__t item__t
+JOIN folio_inventory.holdings_record__t holdings_record__t 
+    ON holdings_record__t.id = item__t.holdings_record_id
+WHERE
+    (
+        item__t.item_level_call_number ~ '[0-9]{3}(.[0-9]+)? [A-Z]{1,2}[0-9]{4,}.*'
+        OR
+        holdings_record__t.call_number ~ '[0-9]{3}(.[0-9]+)? [A-Z]{1,2}[0-9]{4,}.*'
+    ) 
+    AND NOT EXISTS (
+        SELECT
+            1
+        FROM
+            folio_derived.item_notes item_notes
+        WHERE
+            item_notes.item_id = item__t.id
+            AND item_notes.note_type_name = 'Shelving order'
+    )
+$$
+LANGUAGE SQL;
